a8d0cd735e77e7eb939542320f9f03a8a2b0bfdc
anudeepayilalath <anpds816@gmail.com>
2020-10-02 19:14:14 +0200

    updated

--- InClass/A-ES6/exercise1.js
+++ InClass/A-ES6/exercise1.js
@@ -2,24 +2,51 @@
 function arrow() {
   return "es6 is awesome!";
 }
+//answer
+const arrow=()=>{return "es6 is awesome!"}
+
+
+
 
 //Use the es6 syntax for default parameter
 function defaultParameter(name) {
   var name = name || "sam";
   return name;
 }
+//answer
+
+
+
 
 // Use the spread operator to combine arr1 and arr2
 function combineArrays(arr1, arr2) {
   return arr1.concat(arr2);
 }
+//answer
+function combineArrays(arr1, arr2) {
+  return [...arr1, ...arr2];
+}
+
+
+
 
 //use destructuring to return the object's cyf property
 function destructuring(obj) {
   return obj.cyf;
 }
+//answer
+function destructuring(obj) {
+  return { cyf } = obj;
+}
+
+
+
 
 // use template literal to return a string with the sum of a and b
 function templateString(a, b) {
   return "The sum is equal to " + (a + b).toString();
+}
+//answer
+function templateString(a, b) {
+  return `The sum is equal to  + ${a + b.toString()}`;
 }
--- InClass/A-ES6/exercise2.js
+++ InClass/A-ES6/exercise2.js
@@ -34,6 +34,34 @@ console.log('The area of this polygon is ' + r.area);
 
 //Create 2 new classes that extend from polygon: triangle and circle and log their names and areas
 
+class Triangle extends Polygon{
+  constructor(height, width){
+  super(height, width);
+  this.name="triangle";
+  this.area=(width * height) / 2;
+ 
+  }
+ get areaResult(){
+  return this.area
+}
+}
 
+let newTriangle=new Triangle(100, 200)
+
+console.log(newTriangle.areaResult);
+
+
+class Trapezoid extends Polygon{
+  constructor(height, base1, base2){
+  super(height)
+  this.base1=base1
+  this.base2=base2
+  this.name="Trapezoid"
+  this.area=(this.base1 + this.base2) * this.height / 2
+
+}}
+
+let newTrpzd=new Trapezoid(100, 100, 100)
+console.log(newTrpzd.area)
 
 
